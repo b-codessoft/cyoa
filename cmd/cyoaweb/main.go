83f7268da6a99e72355e28cd3f68b20af23b726c
b-codessoft <bruno.dev.codessoft@gmail.com>
2025-09-01 19:29:19 +0500

    Add full video implementation

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/b-codessoft/cyoa"
 )
@@ -26,8 +28,84 @@ func main() {
 		log.Fatal(err)
 	}
 
-	h := cyoa.NewHandler(story, nil)
-	fmt.Printf("Starting the server on port: %d\n", *port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+	tpl := template.Must(template.New("").Parse(storyTmpl))
 
+	h := cyoa.NewHandler(story, cyoa.WithTemplate(tpl), cyoa.WithPathFunc(pathFn))
+
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
+
+	fmt.Printf("Starting the server on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
 }
+
+func pathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+	return path[len("/story/"):]
+}
+
+var storyTmpl = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+		<style>
+			body {
+				font-family: helvetica, arial; 
+			}
+			h1 {
+				text-align: center;
+				position: relative;
+			}
+			.page {
+				width: 80%;
+				max-width: 500px;
+				margin: auto;
+				margin-top: 40px;
+				margin-bottom: 40px;
+				padding: 80px;
+				background: #FFFCF6;
+				border: 1px solid #EEE;
+				box-shadow: 0 10px 6px -6px #777;
+			}
+			ul {
+				border-top: 1px dotted #CCC;
+				padding: 10px 0 0 0;
+				-webkit-padding-start: 0;
+			}
+			li {
+				padding-top: 10px;
+			}
+			a,
+			a:visited {
+				text-decoration: none;
+				color: #6295B5;
+			}
+			a:active,
+			a:hover {
+				color: #7792A2;
+			}
+			p {
+				text-indent: 1em;
+			}
+		</style>
+  </head>
+  <body>
+		<section class="page">
+			<h1>{{.Title}}</h1>
+			{{range .Paragraphs}}
+				<p>{{.}}</p>
+			{{end}}
+			<ul>
+			{{range .Options}}
+				<li><a href="/story/{{.Chapter}}">{{.Text}}</a></li>
+			{{end}}
+			</ul>
+		</section>
+  </body>
+</html>
+`
